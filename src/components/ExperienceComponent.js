1b53fbc71cc54ce634449bcae452577c16e5ffc9
Chipui <chipuikasar@gmail.com>
2022-08-13 18:55:30 +0530

    Added experience

--- src/components/ExperienceComponent.js
+++ src/components/ExperienceComponent.js
@@ -1,7 +1,40 @@
 import React from "react";
-// import styles from "../styles.module.css";
+import styles from "../styles.module.css";
 function ExperienceComponent() {
-  return <>Yet to Update</>;
+  const experiences = [
+    {
+      title: "Sharepoint Developer",
+      company: "Sharepoint Designs",
+      location: "Chennai, Tamil Nadu 600041",
+      date: "July 2021 - Present",
+    },
+  ];
+  return (
+    <>
+      <div className={styles.experience_container}>
+        <h1>Experience</h1>
+        <div className={styles.experience_list}>
+          {experiences.map((experience, index) => {
+            return (
+              <div className={styles.experience_item} key={index}>
+                <div className={styles.experience_item_title}>
+                  <h3>{experience.title}</h3>
+                  <h4>
+                    Company:{" "}
+                    <a href="https://sharepointdesigns.com/">
+                      {experience.company}
+                    </a>
+                  </h4>
+                  <h5>Location: {experience.location}</h5>
+                  <h6>{experience.date}</h6>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ExperienceComponent;
